7481156c46b2c21fe8f24232f2a98eaab8253af7
Pandma13 <mbullard3@gmail.com>
2025-08-13 17:20:48 -0500

    Pushing module-1

---
+++ module-1/MeganFirstProgram.php
@@ -0,0 +1,25 @@
+/**
+ * Megan Wheeler
+ * CSD 440
+ * Module 1
+ * 8/13/2025
+ *
+ * This program is a simple hello world program.
+ *
+*/
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Megan's First PHP Program</title>
+</head>
+
+<body>
+    <?php
+
+    echo "<p>Hello World!</p>";
+
+    ?>
+</body>
+</html>
